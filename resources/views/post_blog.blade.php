78e688c88f3086f1f2402930ce508e31f1295b73
MYLAN\vinh.du <vinh.du@mulangroup.com>
2021-01-28 16:33:43 +0700

    Up UI add_blog

--- resources/views/post_blog.blade.php
+++ resources/views/post_blog.blade.php
@@ -8,15 +8,36 @@
     <p>This is appended to the master sidebar.</p>
 @endsection --}}
 @section('content')
-    <p>This is my body content</p>
-    <textarea name=text id="text" cols="30" rows="10"></textarea>
-    <script src={{ url('ckeditor/ckeditor.js') }}></script>
-    <script>
-    CKEDITOR.replace( 'text', {
-        filebrowserBrowseUrl: '{{ route('ckfinder_browser') }}',
+    <h2 class="title-add-blog">THÊM MỚI BÀI VIẾT</h2>
+    <form>
+        <div class="form-group">
+        <label for="usr">Danh mục</label>
+        <input type="text" class="form-control" id="usr">
+        </div>
+        <div class="form-group">
+        <label for="pwd">Tiêu đề</label>
+        <input type="text" class="form-control" id="pwd">
+        </div>
+        <div class="form-group">
+        <label for="pwd">Ảnh bìa</label>
+        <input type="file" class="form-control" id="pwd">
+        </div>
+        <div class="form-group">
+        <label for="pwd">Nội dung</label>
+        <textarea name=text id="text" cols="30" rows="10"></textarea>
+        <script src={{ url('ckeditor/ckeditor.js') }}></script>
+        <script>
+        CKEDITOR.replace( 'text', {
+            filebrowserBrowseUrl: '{{ route('ckfinder_browser') }}',
 
-    } );
-    </script>
-    @include('ckfinder::setup')
+        } );
+        </script>
+        @include('ckfinder::setup')
+        </div>
+        <div>
+            <button type="button" class="btn btn-success">Thêm</button>
+            <button type="button" class="btn btn-danger">Hủy</button>
+        </div>
+    </form>
 @stop
 
